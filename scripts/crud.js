c23d276683346fe3d18b423fdfc148276395aeb2
Rafael Cassiano <77588479+RafaelCassiano30011@users.noreply.github.com>
2021-04-07 20:56:22 -0300

    Add files via upload

--- scripts/crud.js
+++ scripts/crud.js
@@ -8,7 +8,7 @@ const listaDeParent = {
             id: 2,
             name: 'Jessica',
             idade: 20,
-        }
+        },
     ],
     create: (parent) => {
         listaDeParent.data.push(parent)
@@ -17,13 +17,37 @@ const listaDeParent = {
         return listaDeParent.data
     },
     readbyid: (id) => {
-        for (const parente of listaDeParent.data) {
+        for (const parente of listaDeParent.read()) {
 
             if (id === parente.id) {
                 return parente
-
             }
         }
     },
+    update: (Parentid, data) => {
+        const parentes = listaDeParent.read()
+
+        const ListadeParentesUpdate = parentes.map((parent) => {
+
+            if (Parentid === parent.id) {
+                const novaListadeParentes = {...parent, ...data }
+
+                return novaListadeParentes
+            }
+            return parent
+        })
+        return ListadeParentesUpdate
+    },
+    delete: (ParentId) => {
+        const parentes = listaDeParent.read()
+
+        const deletaParentDaLista = parentes.filter((parent) => {
+            if (ParentId !== parent.id) return true
+            return false
+        })
+        listaDeParent.data = deletaParentDaLista
+
+    },
+
 }
 export default listaDeParent;
--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,11 @@
 import listaDeParent from './crud.js'
-import craterLi from './criarLi.js'
-
-const $inputs = document.querySelectorAll('.input')
-const $button = document.querySelector('.button')
-const $form = document.querySelector('.form-creat')
-const $formId = document.querySelector('.form-read-id')
-const $inputId = document.querySelector('.input-id')
-
-const $container = document.querySelector('.container')
-const $ul = document.querySelector('.ul-box')
 
 
 
-$form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let novoParent = {
-        id: '',
-        name: '',
-        idade: '',
-    }
-
-    novoParent.id = parseInt($inputs[0].value)
-    novoParent.name = $inputs[1].value
-    novoParent.idade = parseInt($inputs[2].value)
 
 
-    listaDeParent.create(novoParent)
-
-    for (const input of $inputs) {
-        input.value = ''
-    }
-    craterLi()
-    console.log(parentes)
-    return
-})
+const update = listaDeParent.update(2, { name: 'joao' })
+const delet = listaDeParent.delete(1)
 
 
-
-$formId.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let id = ''
-
-    id = parseInt($inputId.value)
-
-    const parent = listaDeParent.readbyid(id)
-    const li = `   <li class="li">
-    <Span class="span-id">ID ${parent.id}</Span>
-    <span class="span-nome">Nome ${parent.name}</span>
-    <span class="span-idade">Idade ${parent.idade}</span>
-    </li>`
-
-    $ul.innerHTML = li
-})
-
-
-
-const parent = listaDeParent.readbyid()
+const listaDeParente = listaDeParent.read()
